a67fc4e9fc6659ad669e8200f32ec802edb54640
Bos <planetrizq@gmail.com>
2026-05-07 14:38:15 +0800

    fix: remove unused waitFor import from PromptModal test

--- __tests__/PromptModal.test.tsx
+++ __tests__/PromptModal.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import PromptModal from '@/components/ui/PromptModal'
 import type { VaultFile } from '@/types'
 
